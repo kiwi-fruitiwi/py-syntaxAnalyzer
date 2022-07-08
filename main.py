a03e800133fd9dbc039a4edcd03fa3ad2eb6118b
kiwi-fruitiwi <zane.tian@gmail.com>
2022-07-08 14:25:47 -0700

    add template project and grammar notes

---
+++ main.py
@@ -0,0 +1,37 @@
+# class CompilationEngine {
+# 	compileStatements() {}
+# 	compileIfStatement() {}
+# 	compileWhiteStatement() {}
+# 	...
+# 	compileTerm() {}
+# }
+#
+# jack grammar
+#
+# statement: 		ifStatement | whiteStatement | letStatement
+# statements:		statement*
+# ifStatement:	    if (expression) {statements}
+# whiteStatement:	while (expression) {statements}
+# letStatement:	    let varName = expression;
+# expression:   	term (op term)?
+# term:			    varName | constant
+# varName:		    a string not beginning with a digit
+# constant:		    a decimal number
+# op:				+, -, =, >, <
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
