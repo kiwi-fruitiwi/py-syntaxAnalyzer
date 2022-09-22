ea278ff9ab271d1f2744b29c628df16a19875895
kiwi-fruitiwi <zane.tian@gmail.com>
2022-09-21 23:39:23 -0700

    add planning comments from lecture notes: parsing rules for program structure, statements, expressions

--- parser.py
+++ parser.py
@@ -10,9 +10,43 @@
 #
 # program structure →
 # 	class: 'class' className '{' classVarDec* subroutineDec* '}'
-#
+#	classVarDec: static|field type varName(,varName)*;
+#	type: int char boolean className
+#	subroutineDec: (constructor|function|method)
+#		(void|type) subroutineName(parameterList) subroutineBody
+#	parameterList: (type varName) (, type varName)*)?
+#	subroutineBody: {varDec* statements}
+#	varDec: var type varName (, varName)*;
+#	className: identifier
+#	subroutineName: identifier
+#	varName: identifier
 #
 # statements →
+#	statements: statement*
+#	statement: letStatement ifStatement whileStatement doStatement returnStatement
+#	letStatement: let varName([expression])? = expression;
+#	ifStatement: if(expression){statements}(else{statements})?
+#	whileStatement: while(expression){statements}
+#	doStatement: do subroutineCall;
+#	returnStatement: return expression?;
+#
+# expressions →
+#	expression: term (op term)*
+#	term: integerConstant | stringConstant | keywordConstant | varName |
+#		varName[expression] | subroutineCall | (expression) | unaryOp term
+#	subroutineCall: subName(expressionList) |
+#		(className | varName).subroutineName(expressionList)
+#	expressionList: (expression(, expression)*)?
+#	op: + - * / & | < > =
+#	unaryOp: - ~
+#	keywordConstant: true false null this
+#
+#
+#
+#
+#
+#
+# ? statements → a Jack program includes statements, as follows:
 # 	statement: 		ifStatement | whiteStatement | letStatement
 # 	statements:		statement*
 # 	ifStatement:	if (expression) {statements}
@@ -23,11 +57,6 @@
 # 	varName:		a string not beginning with a digit
 # 	constant:		a decimal number
 # 	op:				+, -, =, >, <
-#
-# expressions
-#	expression: term (op term)*
-#
-#
 #
 
 class CompilationEngine:
